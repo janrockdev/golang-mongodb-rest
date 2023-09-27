62dc963d8001a4459e2549b0c88949f38df25b66
Jan Rock <rock@yottalabs.co.uk>
2023-09-27 22:11:55 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"github.com/ebubekiryigit/golang-mongodb-rest-api-starter/routes"
+	"github.com/ebubekiryigit/golang-mongodb-rest-api-starter/services"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+// @title GoLang Rest API
+// @version 1.0
+// @description GoLang - Gin - RESTful - MongoDB
+// @contact.name Jan Rock
+// @contact.email rock@0xRock.com
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Bearer-Token
+func main() {
+	services.LoadConfig()
+	services.InitMongoDB()
+
+	//if services.Config.UseRedis {
+	//	services.CheckRedisConnection()
+	//}
+
+	routes.InitGin()
+	router := routes.New()
+
+	server := &http.Server{
+		Addr:         services.Config.ServerAddr + ":" + services.Config.ServerPort,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 30,
+		IdleTimeout:  time.Second * 30,
+		Handler:      router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 15 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
